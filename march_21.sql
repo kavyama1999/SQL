d9395c46a521559ab95738c6b1aa02c70d16a4c5
kavyama1999 <kavyama.xworkz@gmail.com>
2024-03-21 09:17:46 +0530

    sql files

---
+++ march_21.sql
@@ -0,0 +1,17 @@
+/*syntax for creating table
+CREATE TABLE table_name(col1 datatype, col datatype, .....);*/
+DDL stmts are related to Structure of the database.
+
+CREATE TABLE gadgets(id int, g_name varchar(20), price bigint);
+
+ SELECT * FROM table_name;
+ 
+ SELECT * FROM gadgets;
+ 
+ Task:
+ CREATE 5 tables with 3 columns(Each table 3 columns);
+ 
+ 
+ 
+ 
+ 
